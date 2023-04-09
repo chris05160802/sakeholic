b443db51aa05bf4171fd4cdd104925f384652692
Chris Wang <chriswang8227@gmail.com>
2023-04-09 16:44:34 +0800

    add new page (poular) and finish all search and all poular

--- javascript/script.js
+++ javascript/script.js
@@ -3,21 +3,22 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
+    var activeCollapsible = document.querySelector('.collapsible.active');
+    if (activeCollapsible && activeCollapsible != this) {
+      activeCollapsible.classList.toggle("active");
+      var activeContent = activeCollapsible.nextElementSibling;
+      activeContent.style.maxHeight = null;
+    }
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-    } 
+    }
   });
 }
 
-const collapseBtn = document.querySelector(".collapsible");
-const collapseContent = document.querySelector(".content");
 
-collapseBtn.addEventListener("click", () => {
-  collapseContent.classList.toggle("show");
-  collapseBtn.classList.toggle("rotate");
-});
+
 
